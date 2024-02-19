7fb578627f737da5d56585c8a3bf659e893f80f8
Héctor Fabio Martínez <hmartinez@buk.co>
2024-02-19 14:59:47 -0500

    Comandos de consola

---
+++ src/Command/ExportBooksInCsvCommand.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Command;
+
+use App\Service\ExportInCsv;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Command\Command;
+
+class ExportBooksInCsvCommand extends Command
+{
+    protected static $defaultName = 'app:books:export-in-csv';
+
+    public function __construct(private ExportInCsv $exportInCsv)
+    {
+        parent::__construct();
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        ($this->exportInCsv)();
+        $output->writeln('CSV wrote successfully');
+        return Command::SUCCESS;
+    }
+}
---
+++ src/Service/ExportInCsv.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Service;
+
+use App\Repository\BookRepository;
+use App\Service\Utils\WriteCsvInterface;
+
+class ExportInCsv
+{
+    public function __construct(
+        private BookRepository $bookRepository,
+        private WriteCsvInterface $writeCsv,
+        private string $projectFolder
+    ) {
+    }
+
+    public function __invoke(): void
+    {
+        $books = $this->bookRepository->findAll();
+        $headers = [
+            'id',
+            'title'
+        ];
+        $contents = [];
+        foreach ($books as $book) {
+            $contents[] = [
+                $book->getId(),
+                $book->getTitle()
+            ];
+        }
+        $this->writeCsv->write($contents, $this->projectFolder . '/var/books.csv', $headers);
+    }
+}
